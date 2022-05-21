5f8b814e58f04e4ef9a4e332d3ae4b970b169d37
tabacegor <tabacegor@gmail.com>
2022-05-21 17:13:09 +0300

    chore: remove unused functions

--- app/src/main/java/com/tabac/ebsintegratortestapp/BaseViewModel.kt
+++ app/src/main/java/com/tabac/ebsintegratortestapp/BaseViewModel.kt
@@ -27,40 +27,4 @@ abstract class BaseViewModel : ViewModel(), LifecycleEventObserver {
      * Override it to handle refresh UI action. The method is triggered from SwipeRefreshLayout.
      */
     open fun onRefresh() {}
-
-    /**
-     * Override it to all android lifecycle dependent logic. The method is called when an activity
-     * is on onCreate or a fragment is on onViewCreated state.
-     */
-    open fun attachView() {}
-
-    /**
-     * Override it to all android lifecycle dependent logic. This method is called when activity or
-     * fragment is onStart state.
-     */
-    open fun onStart() {}
-
-    /**
-     * Override it to all android lifecycle dependent logic. This method is called when activity or
-     * fragment is onPause state.
-     */
-    open fun onPause() {}
-
-    /**
-     * Override it to all android lifecycle dependent logic. This method is called when activity or
-     * fragment is onResume state.
-     */
-    open fun onResume() {}
-
-    /**
-     * Override it to all android lifecycle dependent logic. The method is called when an activity
-     * is on onStop or a fragment is on onViewDestroyed state.
-     */
-    open fun detachView() {}
-
-    /**
-     * Override it to all android lifecycle dependent logic. The method is called when an activity
-     * or a fragment is on onDestroy state.
-     */
-    open fun killView() {}
 }
